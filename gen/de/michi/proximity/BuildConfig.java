bda0785d59eb2e7e67c171a583b8a7539d0457ef
ms122 <michael.schiller@uni-ulm.de>
2013-10-03 16:24:21 +0200

    michis gui

---
+++ gen/de/michi/proximity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.michi.proximity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/de/michi/proximity/Current_Activity.java
@@ -0,0 +1,15 @@
+package de.michi.proximity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+
+public class Current_Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_current);
+	}
+}
